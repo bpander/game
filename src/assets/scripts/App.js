203ebbbb7f152702b398914c6e6f0c6b056acb32
Brad Anderson <brad.anderson87@gmail.com>
2016-12-15 20:43:53 -0600

    Adds boilerplate.

---
+++ src/assets/scripts/App.js
@@ -0,0 +1,12 @@
+import $ from 'jquery';
+
+/**
+ * Application setup
+ *
+ * @class App
+ */
+export default class App {
+    constructor() {
+        $('.js-welcome').text('Welcome to the client-side boilerplate!');
+    }
+}
---
+++ src/assets/scripts/main.js
@@ -0,0 +1,4 @@
+import 'babel-polyfill';
+import App from 'App';
+
+new App();

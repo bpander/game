01f91dfdda89fbbec4d5094b7cc1aea08ec1dfbb
Brad Anderson <brad.anderson87@gmail.com>
2016-12-30 16:28:31 -0600

    Adds preact.

--- src/assets/scripts/App.js
+++ src/assets/scripts/App.js
@@ -1,11 +1,20 @@
+import preact from 'preact';
 
 /**
  * Application setup
  *
  * @class App
  */
-export default class App {
-  constructor() {
+export default class App extends preact.Component {
+
+  constructor(props) {
+    super(props);
     console.log('app started');
+  }
+
+  render() {
+    return (
+      <div>Cupcakes.</div>
+    );
   }
 }
--- src/assets/scripts/main.js
+++ src/assets/scripts/main.js
@@ -1,4 +1,7 @@
 import 'babel-polyfill';
+import preact from 'preact';
 import App from 'App';
 
-window.app = new App();
+
+const appNode = document.getElementById('js-app');
+window.app = preact.render(<App />, appNode);

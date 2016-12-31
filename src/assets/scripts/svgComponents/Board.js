a0a84c55b9cfc3272b6397a382d9af1e6c87c1b4
Brad Anderson <brad.anderson87@gmail.com>
2016-12-30 21:04:19 -0600

    Stubs out how to render rooms and doors.

--- src/assets/scripts/svgComponents/Board.js
+++ src/assets/scripts/svgComponents/Board.js
@@ -34,8 +34,30 @@ export default class Board extends preact.Component {
 
     const { width, height } = data.viewBox.baseVal;
     const { x, y, size } = this.props;
+    const rooms = Array.from(data.getElementById('Rooms').children);
+    const doors = Array.from(data.getElementById('Doors').children);
+
     return (
       <g transform={`translate(${x}, ${y})`}>
+        {doors.map(door => (
+          <rect
+            x={door.x.baseVal.value * size}
+            y={door.y.baseVal.value * size}
+            width={door.width.baseVal.value * size}
+            height={door.height.baseVal.value * size}
+            fill="rebeccapurple"
+          />
+        ))}
+        {rooms.map(room => (
+          <rect
+            x={room.x.baseVal.value * size}
+            y={room.y.baseVal.value * size}
+            width={room.width.baseVal.value * size}
+            height={room.height.baseVal.value * size}
+            fill="none"
+            stroke="green"
+           />
+        ))}
         <Grid width={width} height={height} size={size} />
       </g>
     );
